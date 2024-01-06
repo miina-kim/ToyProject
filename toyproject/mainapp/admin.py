43657e974ba76ad925902fcf7d1c0e86ee85e915
Mina Kim <alsdk4104@gmail.com>
2024-01-07 00:08:40 +0900

    add admin search

--- toyproject/mainapp/admin.py
+++ toyproject/mainapp/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Post # 게시글(Post) Model을 import
 
 # Register your models here.
+
+# 제목으로 데이터 검색
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['postname']
+
 # 관리자(admin)가 게시글(Post)에 접근 가능
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
